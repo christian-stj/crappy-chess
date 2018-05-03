eba411edfeb0cd2cf591700f102f54aec6301841
Måns Anderssone <mans.andersson97@gmail.com>
2018-05-03 13:42:51 +0200

    All pieces are now drawn in their starting positions

--- board.go
+++ board.go
@@ -38,13 +38,40 @@ func UpdateBoard(screen *ebiten.Image, board *[8][8]Tile){
 			img.Fill(tile.color)
 			screen.DrawImage(img, op)
 			if tile.piece != (Piece{}) {
-				screen.DrawImage(tile.piece.pieceIcon, op)
+				opPiece:= &ebiten.DrawImageOptions{}
+				opPiece.GeoM.Scale(0.75,0.75)
+				opPiece.GeoM.Translate(float64(10 + i + i*(b-1)), float64(10 + i + i*(a-1)))
+				screen.DrawImage(tile.piece.pieceIcon, opPiece)
 			}
 		}
 	}
 }
 
 func StartingPiecePos(board *[8][8]Tile){
+	// Draw the "black" pieces
 	board[0][0].piece = CreatePiece(1,3)
+	board[0][1].piece = CreatePiece(1,1)
+	board[0][2].piece = CreatePiece(1,2)
+	board[0][3].piece = CreatePiece(1,5)
+	board[0][4].piece = CreatePiece(1,4)
+	board[0][5].piece = CreatePiece(1,2)
+	board[0][6].piece = CreatePiece(1,1)
+	board[0][7].piece = CreatePiece(1,3)
+	for i:=0; i<8; i++{
+		board[1][i].piece = CreatePiece(1,0)
+	}
+
+	// Draw the "white" pieces
+	board[7][0].piece = CreatePiece(0,3)
+	board[7][1].piece = CreatePiece(0,1)
+	board[7][2].piece = CreatePiece(0,2)
+	board[7][3].piece = CreatePiece(0,5)
+	board[7][4].piece = CreatePiece(0,4)
+	board[7][5].piece = CreatePiece(0,2)
+	board[7][6].piece = CreatePiece(0,1)
+	board[7][7].piece = CreatePiece(0,3)
+	for i:=0; i<8; i++{
+		board[6][i].piece = CreatePiece(0,0)
+	}
 
 }
